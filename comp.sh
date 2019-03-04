9b7e744ca1be7b476a390da3c39679a9e0207c07
ajj31 <syed@siezeconsulting.com>
2019-03-05 00:07:10 +0530

    compile source script mysql

--- comp.sh
+++ comp.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-/home/azz/GitHub/zabbix-4.0.5/configure -enable-server --with-mysql --with-libcurl
+sudo addgroup --system --quiet zabbix
+sudo adduser --quiet --system --disabled-login --ingroup zabbix --home /var/lib/zabbix --no-create-home zabbix
+sudo mkdir -m u=rwx,g=rwx,o= -p /var/lib/zabbix
+chown zabbix:zabbix /var/lib/zabbix
+
+/home/azz/GitHub/zabbix-4.0.5/configure --enable-server --with-postgresql --with-net-snmp 
 sudo make install
 
 
